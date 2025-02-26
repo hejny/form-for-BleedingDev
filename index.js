b625386657b1c18e4f6e36d41880ee671bdcf3fc
Pavol Hejny <me@pavolhejny.com>
2025-02-26 16:16:09 +0100

    Update dependencies and modify feedback handling in form processing

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 
 import { wizzard } from '@promptbook/wizzard';
 
-const result = await wizzard.execute('./books/form.book',{yourName: 'Paul'});
+const result = await wizzard.execute('./books/form.book',{ answers:
+    // { answer1: 'Absolutní pecka' }
+    // { answer1: 'suprdupr workshop, na zacatku trochu pomaly rozjezd a potom naprosta bomba' }
+    // { answer1: 'Pěkný workshop, akorát bych trochu víc ukázal rozdíl mezi GPT-4 a Deepseek' }
+    { answer1: 'Naprosto na hovno' }
+});
 
 // console.info(result);
 
-const { outputParameters: { greeting } } = result;
+const { outputParameters: { email } } = result;
 
-// console.info('---');
-console.info(greeting);
+console.info('=============');
+console.info(email);
+console.info('=============');
