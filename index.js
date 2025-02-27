2ef742283d513f65d6476ee8213642f97f034ac2
Pavol Hejny <me@pavolhejny.com>
2025-02-27 12:18:57 +0100

    Better book

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import { wizzard } from '@promptbook/wizzard';
 const result = await wizzard.execute('./books/form.book',{ answers:
     // { answer1: 'Absolutní pecka' }
     // { answer1: 'suprdupr workshop, na zacatku trochu pomaly rozjezd a potom naprosta bomba' }
-    // { answer1: 'Pěkný workshop, akorát bych trochu víc ukázal rozdíl mezi GPT-4 a Deepseek' }
-    { answer1: 'Naprosto na hovno' }
+     { answer1: 'Pěkný workshop, akorát bych trochu víc ukázal rozdíl mezi GPT-4 a Deepseek' }
+    // { answer1: 'Naprosto na hovno' }
 });
 
 // console.info(result);
